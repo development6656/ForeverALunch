cd437cb40ae67a889e117c6f4ac78d48ef8151e2
Lawrence Yu <lawrenceyu1@gmail.com>
2015-06-13 15:27:18 -0700

    Set up initial routes for airports and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
 
   # root 'welcome#index'
+  root 'session#new'
+
 
   # root 'connection#index'
   # resources :connection
