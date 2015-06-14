2f2d8998cc4953f3e322293558cc9eb66c5b62c9
Lawrence Yu <lawrenceyu1@gmail.com>
2015-06-13 17:02:23 -0700

    Add login/logout/signup/fb oauth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
+ def new
+    @user = User.new
+  end
 
+  def create
+    @user = User.new(
+      username: params[:user][:username],
+      email: params[:user][:email],
+      password: params[:user][:password]
+      )
+    p @user
+      if params[:user][:password] == params[:user][:password_confirmation]
+        @user.save
+        log_in(@user)
+        redirect_to '/sfo/terminals'
+      else
+        @error = "Password confirmation must match Password"
+        render :new
+      end
+
+  end
 end
 
